e8b091862f06d3643b94f2f3fcb3c0d151f6c34e
Emem122-win <knu.ee1202@gmail.com>
2023-05-25 22:03:43 +0900

    🎉 add starter code

---
+++ src/pages/transform-objects/main.js
@@ -0,0 +1,90 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+
+const gui = new dat.GUI();
+
+const canvas = document.querySelector('canvas.webgl');
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene();
+
+/**
+ * Helper
+ */
+const axesHelper = new THREE.AxesHelper(180);
+scene.add(axesHelper);
+
+const gridHelper = new THREE.GridHelper(200, 10);
+scene.add(gridHelper);
+
+/**
+ * Object
+ */
+const sphere = new THREE.SphereGeometry(2, 32, 32);
+const material = new THREE.MeshBasicMaterial({ wireframe: true });
+
+const mesh = new THREE.Mesh(sphere, material);
+scene.add(mesh);
+
+/**
+ * Window Size
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 6;
+camera.updateProjectionMatrix();
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const tick = () => {
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
