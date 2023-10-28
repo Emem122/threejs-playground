a4e2994dbfae1b9f4bc0f3f93d4f66a702e0de56
Emem122 <87292487+Emem122@users.noreply.github.com>
2023-10-28 18:36:46 +0900

    add shader pattern

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,47 @@
-import * as fs from 'fs'
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import * as fs from "fs";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
 
-const root = resolve(__dirname, 'src')
-const pages = resolve(__dirname, 'src', 'pages')
-const outDir = resolve(__dirname, 'dist')
+const root = resolve(__dirname, "src");
+const pages = resolve(__dirname, "src", "pages");
+const outDir = resolve(__dirname, "dist");
 
 // 各ページのディレクトリ名のリストを取得
-const pageDirNameList = fs.readdirSync(pages)
-console.log('pageDirNameList:', pageDirNameList)
+const pageDirNameList = fs.readdirSync(pages);
+console.log("pageDirNameList:", pageDirNameList);
 
 // rollupOptions用のコンフィグを作成
 const pageConfig = pageDirNameList.reduce((arr, pageName) => {
-  arr[pageName] = resolve(root, 'pages', pageName, 'index.html')
-  return arr
-}, {})
+  arr[pageName] = resolve(root, "pages", pageName, "index.html");
+  return arr;
+}, {});
 
 // 各ページ遷移用リストのHTMLを作成
-const paheListHtml = pageDirNameList
-  .map((pageName) => `<li><a href="./pages/${pageName}/index.html">${pageName}</a></li>`)
-  .join('')
+const paheListHtml = pageDirNameList.map((pageName) => `<li><a href="./pages/${pageName}/index.html">${pageName}</a></li>`).join("");
 
 // rollupのプラグインを定義
 // index.htmlを書き換えて各ページへ遷移するリンク用HTMLを差し込む
 const htmlPlugin = () => {
   return {
-    name: 'html-transform',
+    name: "html-transform",
     transformIndexHtml(html) {
-      return html.replace(/<ul id="pageIndex"><\/ul>/, `<ul id="pageIndex">${paheListHtml}</ul>`)
+      return html.replace(/<ul id="pageIndex"><\/ul>/, `<ul id="pageIndex">${paheListHtml}</ul>`);
     },
-  }
-}
+  };
+};
 
 // Viteのコンフィグを定義
 export default defineConfig({
-  root,
+  root: "src/",
   build: {
     outDir,
     rollupOptions: {
       input: {
-        main: resolve(root, 'index.html'),
+        main: resolve(root, "index.html"),
         ...pageConfig,
       },
     },
   },
-  plugins: [htmlPlugin()],
-})
+  plugins: [htmlPlugin(), glsl()],
+});
